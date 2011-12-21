3ab625003131091c4bad030c9281b8b0240272ad
Florian Weber <florian.weber@sfz-bw.de>
2011-12-21 23:26:46 +0100

    bugfixes for Python3 and new highcontrast-colorscheme

--- nicechart.py
+++ nicechart.py
@@ -143,12 +143,10 @@ class NiceChart(inkex.Effect):
 		if(input_type=="\"file\""):
 			csv_file=open(csv_file_name,"r")
 			for line in csv_file:
-				if(line==""):
-					#ignore empty lines:
-					continue
-					value=line.split(csv_delimiter)
+				value=line.split(csv_delimiter)
+				if(len(value)>=2):
 					keys.append(value[col_key])
-					values.append(value[col_val])
+					values.append(float(value[col_val]))
 			csv_file.close()
 		elif(input_type=="\"direct_input\""):
 			what=re.findall("([A-Z|a-z|0-9]+:[0-9]+)",what)
@@ -156,10 +154,6 @@ class NiceChart(inkex.Effect):
 				value=value.split(":")
 				keys.append(value[0])
 				values.append(float(value[1]))
-		else:
-			err_log=open("/home/florian/err.log","a")
-			err_log.write("Error: input_type="+input_type+"\n")
-			err_log.close()
 
 		# Get script's "--type" option value.
 		charttype=self.options.type
@@ -213,7 +207,7 @@ class NiceChart(inkex.Effect):
 				value_max=0.0
 
 			for x in range(len(values)):
-				values[x]=(float(values[x])/value_max)*bar_height
+				values[x]=(values[x]/value_max)*bar_height
 			
 			# Get defs of Document
 			defs = self.xpathSingle('/svg:svg//svg:defs')
--- nicechart_colors.py
+++ nicechart_colors.py
@@ -2,6 +2,7 @@
 red= ["#460101","#980101","#d40000","#f44800","#fb8b00","#eec73e","#d9bb7a","#fdd99b"]
 blue=["#000442","#0F1781","#252FB7","#3A45E1","#656DDE","#8A91EC"]
 gray=["#222222","#444444","#666666","#888888","#aaaaaa","#cccccc","#eeeeee"]
+contrast=["#0000FF","#FF0000","#00FF00","#CF9100","#FF00FF","#00FFFF"]
 default=red
 
 def get_color_scheme(name):
@@ -13,5 +14,7 @@ def get_color_scheme(name):
 		return blue
 	elif(name=="gray"):
 		return gray
+	elif(name=="contrast"):
+		return contrast
 	else:
 		return default
